a5d6a17df351a3c46c429aa06ac939b41b555cce
smileythf <52077852+smileythf@users.noreply.github.com>
2020-05-25 14:22:26 +0100

    dsc

---
+++ DSC/CreateADPDC.ps1
@@ -0,0 +1,99 @@
+﻿configuration Create1stADDC
+{
+   param
+   (
+        [Parameter(Mandatory)]
+        [String]$DomainName,
+
+        [Parameter(Mandatory)]
+        [String]$username,
+
+        [Parameter(Mandatory)]
+        [String]$pwd,
+
+        [Int]$RetryCount=20,
+        [Int]$RetryIntervalSec=30
+    )
+
+    [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\${username}, ${pwd})
+
+    Node localhost
+    {
+        LocalConfigurationManager
+        {
+            RebootNodeIfNeeded = $true
+        }
+
+        WindowsFeature DNS
+        {
+            Ensure = "Present"
+            Name = "DNS"
+        }
+
+        WindowsFeature DnsTools
+        {
+            Ensure = "Present"
+            Name = "RSAT-DNS-Server"
+            DependsOn = "[WindowsFeature]DNS"
+        }
+
+        xDnsServerAddress DnsServerAddress
+        {
+            Address        = '127.0.0.1'
+            InterfaceAlias = Ethernet
+            AddressFamily  = 'IPv4'
+            DependsOn = "[WindowsFeature]DNS"
+        }
+
+        xWaitforDisk Disk2
+        {
+            DiskNumber = 2
+            RetryIntervalSec =$RetryIntervalSec
+            RetryCount = $RetryCount
+        }
+
+        xDisk ADDataDisk {
+            DiskNumber = 2
+            DriveLetter = "F"
+            DependsOn = "[xWaitForDisk]Disk2"
+        }
+
+        WindowsFeature ADDSInstall
+        {
+            Ensure = "Present"
+            Name = "AD-Domain-Services"
+            DependsOn="[WindowsFeature]DNS"
+        }
+
+        WindowsFeature ADDSTools
+        {
+            Ensure = "Present"
+            Name = "RSAT-ADDS-Tools"
+            DependsOn = "[WindowsFeature]ADDSInstall"
+        }
+
+        WindowsFeature ADAdminCenter
+        {
+            Ensure = "Present"
+            Name = "RSAT-AD-AdminCenter"
+            DependsOn = "[WindowsFeature]ADDSTools"
+        }
+
+        xADDomain FirstDS
+        {
+            DomainName = $DomainName
+            DomainAdministratorCredential = $DomainCreds
+            SafemodeAdministratorPassword = $DomainCreds
+            DatabasePath = "F:\NTDS"
+            LogPath = "F:\NTDS"
+            SysvolPath = "F:\SYSVOL"
+            DependsOn = @("[WindowsFeature]ADDSInstall", "[xDisk]ADDataDisk")
+        }
+
+        xPendingReboot RebootAfterPromotion{
+            Name = "RebootAfterPromotion"
+            DependsOn = "[xADDomain]FirstDS"
+        }
+
+   }
+}
